95e1845a0424c623c5d1cf2ddc26f3dbcce20546
NanamiArihara <nanami_arihara@outlook.com>
2018-08-12 18:25:58 +0800

    Fixed #16

--- src/main/java/net/infstudio/goki/common/CommonProxy.java
+++ src/main/java/net/infstudio/goki/common/CommonProxy.java
@@ -4,6 +4,7 @@ import net.infstudio.goki.GokiStats;
 import net.infstudio.goki.client.gui.GuiHandler;
 import net.infstudio.goki.common.handlers.CommonHandler;
 import net.infstudio.goki.common.handlers.TickHandler;
+import net.infstudio.goki.common.init.GokiSounds;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 
@@ -13,6 +14,7 @@ public class CommonProxy {
 
     public void registerHandlers() {
         MinecraftForge.EVENT_BUS.register(new GokiStats());
+        MinecraftForge.EVENT_BUS.register(new GokiSounds());
         MinecraftForge.EVENT_BUS.register(new CommonHandler());
         MinecraftForge.EVENT_BUS.register(new TickHandler());
 
