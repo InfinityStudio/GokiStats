e7fcbd5f39ca7d2b8dca8f0f48ecc412cae6df02
CI010 <cijhn@hotmail.com>
2018-09-15 01:24:21 -0700

    add revert skill limit; revert skill return exp is configurable now; (#24)

--- src/main/java/net/infstudio/goki/common/network/packet/PacketStatAlter.java
+++ src/main/java/net/infstudio/goki/common/network/packet/PacketStatAlter.java
@@ -4,6 +4,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import net.infstudio.goki.GokiStats;
 import net.infstudio.goki.common.utils.DataHelper;
+import net.infstudio.goki.common.config.GokiConfig;
 import net.infstudio.goki.common.stats.StatBase;
 import net.infstudio.goki.common.stats.StatMaxHealth;
 import net.minecraft.entity.SharedMonsterAttributes;
@@ -40,50 +41,57 @@ public class PacketStatAlter implements GokiPacket {
 
     @Override
     public void handleServerSide(EntityPlayer player) {
-        if (player != null) {
-            StatBase stat = StatBase.stats.get(this.stat);
-            if (stat.enabled) {
-                int level = DataHelper.getPlayerStatLevel(player, stat);
-                if (this.amount > 0) {
-                    int cost = stat.getCost(level + this.amount - 1);
-                    int currentXP = DataHelper.getXPTotal(player.experienceLevel,
-                            player.experience);
+        if (player == null)
+            return;
+        StatBase stat = StatBase.stats.get(this.stat);
+        if (!stat.enabled)
+            return;
+        int level = DataHelper.getPlayerStatLevel(player, stat);
+        if (this.amount > 0) {
 
-                    if (stat.enabled) {
-                        if (level + this.amount > stat.getLimit()) {
-                            this.amount = 0;
-                        }
-
-                        if ((currentXP >= cost) && (this.amount != 0)) {
-                            DataHelper.setPlayerStatLevel(player,
-                                    stat,
-                                    level + this.amount);
-                            if (stat instanceof StatMaxHealth) {
-                                player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20 + level + this.amount);
-                            }
-
-                            if (this.amount > 0) {
-                                DataHelper.setPlayersExpTo(player, currentXP - cost);
-                            }
-                        }
-                    }
-                } else if (DataHelper.getPlayerStatLevel(player, stat) > 0) {
-                    player.addExperience((int) (stat.getCost(level + this.amount - 2) * 0.8)); // TODO Configure 0.8
-                    DataHelper.setPlayerStatLevel(player,
-                            stat,
-                            level + this.amount);
-                    if (stat instanceof StatMaxHealth) {
-                        player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20 + level + this.amount);
-                    }
-                }
+            if (level + this.amount > stat.getLimit()) {
+                this.amount = stat.getLimit() - level;
             }
-//			else if (this.amount >= 0)
-//				;
 
-            GokiStats.packetPipeline.sendTo(new PacketStatSync(player),
-                    (EntityPlayerMP) player);
-            GokiStats.packetPipeline.sendTo(new PacketSyncXP(player),
-                    (EntityPlayerMP) player);
+            int cost = stat.getCost(level + this.amount - 1);
+            int currentXP = DataHelper.getXPTotal(player.experienceLevel, player.experience);
+
+            if (currentXP >= cost) {
+                int reverted = DataHelper.getPlayerRevertStatLevel(player, stat);
+                reverted = Math.max(reverted - this.amount, 0);
+                DataHelper.setPlayerRevertStatLevel(player, stat, reverted);
+
+                DataHelper.setPlayerStatLevel(player, stat, level + this.amount);
+                if (stat instanceof StatMaxHealth) {
+                    player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH)
+                            .setBaseValue(20 + level + this.amount);
+                }
+
+                DataHelper.setPlayersExpTo(player, currentXP - cost);
+            }
+
+            GokiStats.packetPipeline.sendTo(new PacketStatSync(player), (EntityPlayerMP) player);
+            GokiStats.packetPipeline.sendTo(new PacketSyncXP(player), (EntityPlayerMP) player);
+        } else if (DataHelper.getPlayerStatLevel(player, stat) > 0) {
+
+            int reverted = DataHelper.getPlayerRevertStatLevel(player, stat);
+            int maxRevert = GokiConfig.globalModifiers.globalMaxRevertLevel;
+
+            if (maxRevert > 0 && reverted + (-this.amount) > maxRevert) { // check if we limit the max revert, and if the revert overflow the value
+                this.amount = -(maxRevert - reverted); // ceil the value for max revert
+            }
+
+            reverted += (-this.amount);
+            DataHelper.setPlayerRevertStatLevel(player, stat, reverted);
+
+            player.addExperience((int) (stat.getCost(level + this.amount - 2) * GokiConfig.globalModifiers.globalRevertFactor)); 
+            DataHelper.setPlayerStatLevel(player, stat, level + this.amount);
+            if (stat instanceof StatMaxHealth) {
+                player.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(20 + level + this.amount);
+            }
+
+            GokiStats.packetPipeline.sendTo(new PacketStatSync(player), (EntityPlayerMP) player);
+            GokiStats.packetPipeline.sendTo(new PacketSyncXP(player), (EntityPlayerMP) player);
         }
     }
 }
