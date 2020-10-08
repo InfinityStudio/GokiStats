c9d31bc1c5b5b55c5a8353b29753d8963e8fd47a
Lasm Gratel <lasm_gratel@hotmail.com>
2020-10-08 18:42:57 +0800

    Fix #56 Fix a bug while reverting

--- src/main/java/net/infstudio/goki/client/network/handler/PacketSyncClientHandler.java
+++ src/main/java/net/infstudio/goki/client/network/handler/PacketSyncClientHandler.java
@@ -1,6 +1,5 @@
 package net.infstudio.goki.client.network.handler;
 
-import net.infstudio.goki.GokiStats;
 import net.infstudio.goki.api.stat.StatBase;
 import net.infstudio.goki.api.stat.Stats;
 import net.infstudio.goki.common.network.message.S2CStatSync;
@@ -9,6 +8,9 @@ import net.infstudio.goki.common.utils.DataHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.network.NetworkEvent;
 
 import java.util.function.Supplier;
@@ -16,39 +18,47 @@ import java.util.function.Supplier;
 public class PacketSyncClientHandler {
     public static void acceptSync(S2CStatSync message, Supplier<NetworkEvent.Context> context) {
         NetworkEvent.Context ctx = context.get();
-        PlayerEntity player = Minecraft.getInstance().player;
-        if (player == null)
-            return;
-        StatBase stat = StatBase.stats.get(message.stat);
         ctx.enqueueWork(() -> {
-            if (stat == Stats.MAX_HEALTH)
-                player.getAttribute(SharedMonsterAttributes.MAX_HEALTH)
-                        .setBaseValue(20 + message.amount);
-            DataHelper.setPlayerRevertStatLevel(player, stat, message.reverted);
-            DataHelper.setPlayerStatLevel(player, stat, message.amount);
-            GokiStats.log.debug("Loaded stat from server.");
+            DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> acceptSyncInternal(message));
         });
         ctx.setPacketHandled(true);
     }
 
     public static void acceptSyncAll(S2CSyncAll message, Supplier<NetworkEvent.Context> context) {
         NetworkEvent.Context ctx = context.get();
+        ctx.enqueueWork(() -> {
+            DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> acceptSyncAllInternal(message));
+        });
+        ctx.setPacketHandled(true);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void acceptSyncInternal(S2CStatSync message) {
         PlayerEntity player = Minecraft.getInstance().player;
         if (player == null)
             return;
-        ctx.enqueueWork(() -> {
-            for (int i = 0; i < message.statLevels.length; i++) {
-                DataHelper.setPlayerStatLevel(player,
-                        StatBase.stats.get(i),
-                        message.statLevels[i]);
-            }
-            for (int i = 0; i < message.revertedStatLevels.length; i++) {
-                DataHelper.setPlayerRevertStatLevel(player, StatBase.stats.get(i), message.revertedStatLevels[i]);
-            }
+        StatBase stat = StatBase.stats.get(message.stat);
+        if (stat == Stats.MAX_HEALTH)
             player.getAttribute(SharedMonsterAttributes.MAX_HEALTH)
-                    .setBaseValue(20 + DataHelper.getPlayerStatLevel(player, Stats.MAX_HEALTH));
-            GokiStats.log.debug("Loaded stats from server.");
-        });
-        ctx.setPacketHandled(true);
+                    .setBaseValue(20 + message.amount);
+        DataHelper.setPlayerRevertStatLevel(player, stat, message.reverted);
+        DataHelper.setPlayerStatLevel(player, stat, message.amount);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void acceptSyncAllInternal(S2CSyncAll message) {
+        PlayerEntity player = Minecraft.getInstance().player;
+        if (player == null)
+            return;
+        for (int i = 0; i < message.statLevels.length; i++) {
+            DataHelper.setPlayerStatLevel(player,
+                    StatBase.stats.get(i),
+                    message.statLevels[i]);
+        }
+        for (int i = 0; i < message.revertedStatLevels.length; i++) {
+            DataHelper.setPlayerRevertStatLevel(player, StatBase.stats.get(i), message.revertedStatLevels[i]);
+        }
+        player.getAttribute(SharedMonsterAttributes.MAX_HEALTH)
+                .setBaseValue(20 + DataHelper.getPlayerStatLevel(player, Stats.MAX_HEALTH));
     }
 }
